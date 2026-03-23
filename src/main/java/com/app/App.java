47f4d19a04d73516ad61dfc5af913131b6d0d1b3
EC2 Default User <ec2-user@ip-172-31-37-103.ap-south-1.compute.internal>
2026-03-23 06:28:50 +0000

    test ci cd update

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,13 +1,20 @@
+
 package com.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Hello DevOps CI/CD 🚀</h1>";
     }
 }
